edad1d895ddbb39e3be414801d013593fb65b40b
Naveenkumar <naveent1508@gmail.com>
2024-02-13 08:48:41 +0530

    Method overriding file added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.meth();
+        obj.meth();
+    }
 }
